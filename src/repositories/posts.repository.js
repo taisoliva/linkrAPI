fed990d95cfe895024206864a38684e4996035ac
joao <joaosb004@gmail.com>
2023-05-30 19:28:25 -0300

    fix/ get user profile

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -2,7 +2,7 @@ import pool from "../configs/dbConn.js";
 
 export async function findPostsByUserId(id){
     const client = await pool.connect();
-    const data = pool.query(`SELECT link, description, likes FROM posts WHERE user_id=$1`,[id]);
+    const data = pool.query(`SELECT link, description, likes FROM post WHERE user_id=$1`,[id]);
     client.release();
     return data;
 }
