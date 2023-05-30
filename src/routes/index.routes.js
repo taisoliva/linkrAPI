c14968e9027ea714d3c49a9af123ec4f508b8514
joao <joaosb004@gmail.com>
2023-05-30 18:18:11 -0300

    feat/ user/:id route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,6 +5,10 @@ import authRouter from "./auth.js";
 const router = Router();
 
 router.use(usersRouter);
+<<<<<<< HEAD
 router.use("/" ,authRouter);
+=======
+router.use(authRouter);
+>>>>>>> 40b4d7c (feat/ user/:id route)
 
 export default router;
