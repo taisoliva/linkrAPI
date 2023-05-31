41315c691ec51ceb40ea8df669d28149e977ad1d
joao <joaosb004@gmail.com>
2023-05-30 21:20:58 -0300

    feat/ edit and delete posts

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -1,8 +1,27 @@
 import pool from "../configs/dbConn.js";
 
-export async function findPostsByUserId(id){
+export async function findPostsByUserId(id) {
     const client = await pool.connect();
-    const data = pool.query(`SELECT link, description, likes FROM post WHERE user_id=$1`,[id]);
+    const data = pool.query(`SELECT id, link, description, likes FROM post WHERE user_id=$1`, [id]);
     client.release();
     return data;
+}
+
+export async function getPostById(id) {
+    const client = await pool.connect();
+    const data = pool.query(`SELECT id, user_id FROM post WHERE id=$1`, [id]);
+    client.release();
+    return data;
+}
+
+export async function modifyPost(description, id) {
+    const client = await pool.connect();
+    pool.query(`UPDATE post SET description=$1 WHERE id=$2`, [description, id]);
+    client.release();
+}
+
+export async function nukePost (id) {
+    const client = await pool.connect();
+    pool.query(`DELETE FROM post WHERE id=$1`, [id]);
+    client.release();
 }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import usersRouter from "./users.routes.js";
 import authRouter from "./auth.js";
+import postsRouter from "./posts.routes.js";
 
 const router = Router();
 
 router.use(usersRouter);
 router.use("/" ,authRouter);
+router.use(postsRouter);
 
 export default router;
