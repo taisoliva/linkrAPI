c0b696d11301e042e391638804a63ce7cbdcf2e1
tais <tais15oliva@gmail.com>
2023-05-31 10:50:59 -0300

    fix: corrigindo nome da table posts

--- src/repositories/publishRepository.js
+++ src/repositories/publishRepository.js
@@ -1,11 +1,11 @@
 import pool from "../configs/dbConn.js";
 
-export async function createLinkDB(url, description, id) {
+export async function createLinkDB(url, description, avatar, id) {
     const client = await pool.connect()
     try {
-        return client.query(` INSERT INTO post (link, description, user_id) 
-                            VALUES ($1,$2,$3)
-    `, [url, description, id])
+        return client.query(` INSERT INTO posts (link, description, user_id, picture) 
+                            VALUES ($1,$2,$3, $4)
+    `, [url, description, id, avatar])
     } catch (err) {
         console.error("Error updating refresh token", err);
         throw err;
@@ -18,8 +18,8 @@ export async function createLinkDB(url, description, id) {
 export async function getPostDB() {
     const client = await pool.connect()
     try {
-        return client.query(` SELECT post.*, users.name FROM post
-                              JOIN users ON users.id = post.user_Id  
+        return client.query(` SELECT posts.*, users.name FROM posts
+                              JOIN users ON users.id = posts.user_Id  
                               ORDER BY id DESC 
                               LIMIT 20`)
     } catch (err) {
