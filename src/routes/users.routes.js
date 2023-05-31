a1258ae6ddc9384f37cf977af132d6c685a2020d
taisoliva <89030631+taisoliva@users.noreply.github.com>
2023-05-31 10:28:10 -0300

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,9 @@
 import { Router } from "express";
 import { validateIdAsParams } from "../middlewares/validateParams.js";
-import { getUserProfileById, logout } from "../controllers/users.controller.js";
 import { getUserProfileById, getUserByName } from "../controllers/users.controller.js";
 import verfifyJWT from "../middlewares/verifyJWT.js";
 
 const usersRouter = Router();
-
-usersRouter.get("/user/:id", verfifyJWT, validateIdAsParams, getUserProfileById);
-usersRouter.post("/logout", verfifyJWT, logout);
 
 usersRouter.get("/getById/:id", verfifyJWT, validateIdAsParams, getUserProfileById);
 usersRouter.get("/getByName/:name", verfifyJWT, getUserByName);
