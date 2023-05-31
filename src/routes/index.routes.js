96be468070e5fe3ecdbc3e4ae5f14fdef3751263
tais <tais15oliva@gmail.com>
2023-05-31 10:04:48 -0300

    feat/publish

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import usersRouter from "./users.routes.js";
 import authRouter from "./auth.js";
+import publishRouter from "./publish.js";
 
 const router = Router();
 
 router.use(usersRouter);
 router.use("/" ,authRouter);
+router.use("/publish", publishRouter)
 
 export default router;
