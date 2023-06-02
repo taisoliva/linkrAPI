2543dbbc7f2bb1c8155fec505498c77fb9f7ce53
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-06-02 20:28:40 -0300

    style: Format files to adhere to coding style standards

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -1,15 +1,18 @@
 import pool from "../configs/dbConn.js";
 
-export async function findProfileByUserId(id){
-    const client = await pool.connect();
-    const data = pool.query(`SELECT name, picture FROM users WHERE id=$1`,[id]);
-    client.release();
-    return data;
+export async function findProfileByUserId(id) {
+  const client = await pool.connect();
+  const data = pool.query(`SELECT name, picture FROM users WHERE id=$1`, [id]);
+  client.release();
+  return data;
 }
 
-export async function findUserByName(name){
-    const client = await pool.connect();
-    const data = pool.query(`SELECT id, name, picture FROM users WHERE name LIKE $1 `,[`%${name}%`]);
-    client.release();
-    return data;
+export async function findUserByName(name) {
+  const client = await pool.connect();
+  const data = pool.query(
+    `SELECT id, name, picture FROM users WHERE name LIKE $1 `,
+    [`%${name}%`]
+  );
+  client.release();
+  return data;
 }
