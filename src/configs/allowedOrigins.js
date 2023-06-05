7b5ae18d0beb4dfc4dff5e1c6a076e5682913789
joao <joaosb004@gmail.com>
2023-06-05 17:17:09 -0300

    fix deploy

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -5,8 +5,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
-  "https://projeto18-freela-front-theta.vercel.app/",
-  "https://projeto18-freela-front-theta.vercel.app",
+  "https://linkr-iota.vercel.app",
   "http://react-server:8000",
   "https://react-server:8000",
   "http://localhost:8000,"
