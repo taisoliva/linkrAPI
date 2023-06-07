477aac62e3299a146807b62feea4cfd962865723
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-06-07 19:45:05 -0300

    now u can follow a user

--- src/controllers/follow.controller.js
+++ src/controllers/follow.controller.js
@@ -2,10 +2,11 @@ import process from "../repositories/follow.repository.js";
 export async function follow(req, res) {
   const userId = res.locals.user.id;
   const friendId = req.body.id;
+  if (userId == friendId) return res.status(400).send("You can't follow yourself");
   try {
-    await process.follow(req.body.id);
+    await process.follow(userId, friendId);
 
-    return res.send(ok);
+    return res.status(200).send();
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -14,10 +15,11 @@ export async function follow(req, res) {
 export async function unfollow(req, res) {
   const userId = res.locals.user.id;
   const friendId = req.body.id;
+  if (userId == friendId) return res.status(400).send("You can't unfollow yourself");
   try {
-    await process.unfollow(req.params.id);
+    await process.unfollow(userId, friendId);
 
-    return res.send(ok);
+    return res.status(200).send();
   } catch (err) {
     res.status(500).send(err.message);
   }
