fe725a778f97347f54f23d20aa0e61775901f732
joao <joaosb004@gmail.com>
2023-06-08 16:59:17 -0300

    feat/ infiniteScroll

--- src/schemas/publish.schema.js
+++ src/schemas/publish.schema.js
@@ -2,7 +2,7 @@ import joi from "joi";
 
 const publishShema = joi.object({
   url: joi.string().uri().required(),
-  description: joi.string().allow(""),
+  description: joi.string().allow("").required(),
 });
 
 export default publishShema;
