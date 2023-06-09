42a608e78f13290448087a892da39025ba3b4fc5
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-06-08 23:10:11 -0300

    feat: get follows end-point scoope

--- src/controllers/follows.controller.js
+++ src/controllers/follows.controller.js
@@ -1,8 +1,10 @@
 import process from "../repositories/follows.repository.js";
+
 export async function follow(req, res) {
   const userId = res.locals.user.id;
   const friendId = req.body.id;
-  if (userId == friendId) return res.status(400).send("You can't follow yourself");
+  if (userId == friendId)
+    return res.status(400).send("You can't follow yourself");
   try {
     await process.follow(userId, friendId);
 
@@ -15,7 +17,8 @@ export async function follow(req, res) {
 export async function unfollow(req, res) {
   const userId = res.locals.user.id;
   const friendId = req.body.id;
-  if (userId == friendId) return res.status(400).send("You can't unfollow yourself");
+  if (userId == friendId)
+    return res.status(400).send("You can't unfollow yourself");
   try {
     await process.unfollow(userId, friendId);
 
@@ -24,3 +27,11 @@ export async function unfollow(req, res) {
     res.status(500).send(err.message);
   }
 }
+
+export const getFollows = async (_, res) => {
+  const userId = res.locals.user.id;
+  try {
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
